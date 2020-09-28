9adaa331b6512bccc9da6a7cf976d6170e2af72d
Baron Liu <bbaronliu@gmail.com>
2020-09-28 03:35:12 -0700

    fixed 3

--- js/ex3.js
+++ js/ex3.js
@@ -11,18 +11,21 @@ function add() {
     var sum = x.reduce(function(a, b ) {
         return a + b ;
     });
+    return sum
 }
 
 function biggest() {
     var max = x.reduce(function(a, b) {
         return Math.max(a, b)
     });
+    return max
 }
 
 function smallest() {
     var min = x.reduce(function(a, b) {
         return Math.min(a, b)
     });
+    return min
 }
 
 console.log("The Sum of the Values is " + add());
