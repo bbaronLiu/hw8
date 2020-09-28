ec87dc550d7e6ace5b2fcc09b09f720190c9f5b6
Baron Liu <bbaronliu@gmail.com>
2020-09-28 03:31:43 -0700

    numbernotadding2

--- js/ex1.js
+++ js/ex1.js
@@ -3,8 +3,8 @@
 */
 console.log("Baron's Output from Tutorial 4 Example 1");
 
-var Number(number1) = prompt("Enter Number1: ")
-var Number(number2) = prompt("Enter Number 2: ")
+var number1 = Number(prompt("Enter Number1: "))
+var number2 = Number(prompt("Enter Number 2: "))
 var math = prompt("Enter Mathematical Operation")
 
 function solve(number1, number2, math) {
