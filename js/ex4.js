c0ee58a3d3a9bac051579de684a74e7be620d6a0
Baron Liu <bbaronliu@gmail.com>
2020-09-28 03:19:35 -0700

    fix4v2

--- js/ex4.js
+++ js/ex4.js
@@ -3,28 +3,28 @@ Example 2 JavaScript code
 */
 console.log("Baron's Output from Tutorial 4 Example 4");
 
-var x = "stop"
-var z = []
-var counter = 0
+let y;
+var z = [];
+var counter = 0;
 
 
 function words() {
-while (x != "stop") {
-    var y = prompt("enter a word: ")
-    if (y.toLowerCase() == "stop") {
-        if (counter == 0) {
-            console.log("No words to display, Stop was the first word entered");
-        } else {
-            console.log("You entered the following words:")
-            for (var i in z) {
+    while (y != "stop") {
+        var y = prompt("enter a word: ")
+        if (y.toLowerCase() == "stop") {
+            if (counter == 0) {
+                console.log("No words to display, Stop was the first word entered");
+            } else {
+                console.log("You entered the following words:")
+                for (var i in z) {
                 console.log(i);
+                }
             }
+        } else {
+            z.push(y);
+            counter = counter + 1;
         }
-    } else {
-        z.push(y);
-        counter = counter + 1;
     }
-}
 }
 
 words()
