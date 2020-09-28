bb4b40e08e78c3268d58eacf893ca467fb4d1521
Baron Liu <bbaronliu@gmail.com>
2020-09-28 02:04:06 -0700

    ex2

--- js/ex2.js
+++ js/ex2.js
@@ -2,3 +2,17 @@
 Example 2 JavaScript code
 */
 console.log("Asher's Output from Tutorial 4 Example 2");
+
+const r = Number(prompt("Enter the circle radius:"));
+
+var circle = {};
+circle.radius = r;
+circle.circumference = function {
+    return Math.PI * 2 * this.radius;
+}
+circle.area = function {
+    return Math.PI * this.radius * this.radius;
+}
+
+console.log(`Its circumference is ${circle.circumference()}`);
+console.log(`Its area is ${circle.area()}`);
